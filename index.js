968f6097e6d783447a5ae768d809ac6836d6e1ed
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2025-09-04 10:39:10 -0300

    aaaa

--- index.js
+++ index.js
@@ -5,14 +5,28 @@ const contenedor =document.getElementById("contenedor-resultado");
 const boton = document.getElementById("mostrar");
 boton.addEventListener("click", mostrar);
 
+let contadorDivs=0;
 function mostrar() {
  const resultadoinput= document.createElement("h1");
-  resultadoinput.innerText= textoInput.value;
-  contenedor.appendChild(resultadoinput);
+   resultadoinput.innerText= textoInput.value;
+   resultadoinput.classList.add("resultadosinput")
+   
   const resultadotext= document.createElement("p");
-  resultadotext.innerText= textoTextarea.value;
-  contenedor.appendChild(resultadotext);
- resultadoinput.classList.toggle("resultadosinput")
- resultadotext.classList.toggle("resultadostext")
+    resultadotext.innerText= textoTextarea.value;
+    resultadotext.classList.add("resultadostext")
+
+ const respuesta= document.createElement("div");
+   respuesta.classList.add("resultado");
+   
+ 
+   contadorDivs++;
+   respuesta.id = `respuesta-${contadorDivs}`;
+
+    respuesta.appendChild(resultadoinput);
+    respuesta.appendChild(resultadotext);
+
+    contenedor.appendChild(respuesta);
+    
+    
 }
  
